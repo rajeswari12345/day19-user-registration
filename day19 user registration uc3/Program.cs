88847a4c17bee14709861974d1dba7187de48e7d
mani1 <mani1@NANI143>
2022-07-03 21:59:20 +0530

    day19 user registration

---
+++ day19 user registration uc3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.RegularExpressions;
+public class Validmail
+{
+
+    public static Regex _ regex =  new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+     Match match = regex.Match(email);
+
+
+    public static bool IsValidEmailFormat(string emailInput)
+    {
+        return _regex.IsMatch(emailInput);
+    }
+}
---
+++ day19 user registration/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.RegularExpressions;
+namespace FirstName
+{
+
+
+    // Function to validate the username
+    public static bool isValidFirstName(String firstname)
+    {
+
+        // Regex to check valid firstname.
+        String regex = "^[A-Za-z]\\w{5,29}$";
+
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+
+        // If the username is empty
+        // return false
+        if (name == null)
+        {
+            return false;
+        }
+
+        // Pattern class contains matcher() method
+        // to find matching between given username
+        // and regular expression.
+        Matcher m = p.matcher(name);
+
+        // Return if the username
+        // matched the ReGex
+        return m.matches();
+    }
+
+    // Driver Code
+    public static void Main(String[] args)
+    {
+
+        // Test Case: 1
+        String str = "honey";
+        Console.WriteLine(isValidFirstName(str));
+
+    }
+}
+}
